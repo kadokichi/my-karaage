7e4727f7b474e32c8a7cca0406ec6a416c234af2
github kadokichi <dydy2929@yahoo.co.jp>
2024-08-07 12:40:15 +0900

    バリデーション追加

--- app/models/nice.rb
+++ app/models/nice.rb
@@ -2,4 +2,7 @@ class Nice < ApplicationRecord
   belongs_to :user
   belongs_to :review
   counter_culture :review, column_name: "nices_count", touch: true
+
+  validates :user, presence: true
+  validates :review, presence: true
 end
